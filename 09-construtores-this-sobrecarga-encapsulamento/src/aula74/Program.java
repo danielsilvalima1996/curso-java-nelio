6bb2a047c384b503b7e01d61d4e460527d3ccba1
Daniel da Silva de Lima <dsl15021996@gmail.com>
2020-04-14 21:46:58 -0300

    aula 78, continuar

--- 09-construtores-this-sobrecarga-encapsulamento/src/aula74/Program.java
+++ 09-construtores-this-sobrecarga-encapsulamento/src/aula74/Program.java
@@ -8,35 +8,36 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 
-		Product produto = new Product();
 
 		System.out.println("Digite os dados do produto: ");
 		System.out.print("Nome: ");
-		produto.name = sc.nextLine();
-		
+		String name = sc.nextLine();
+
 		System.out.print("Preço: ");
-		produto.price = sc.nextDouble();
-		
+		double price = sc.nextDouble();
+
 		System.out.print("Quantidade no estoque: ");
-		produto.quantity = sc.nextInt();
-		System.out.println("");
-		
-		System.out.println("Dados do Produto: " + produto.toString());
-		System.out.println("");
-		
-		System.out.print("Digite o número de produtos para adicionar ao estoque: ");
 		int quantity = sc.nextInt();
 		System.out.println("");
-		produto.addProducts(quantity);
 		
+		Product produto = new Product(name, price, quantity);
+
+		System.out.println("Dados do Produto: " + produto.toString());
+		System.out.println("");
+
+		System.out.print("Digite o número de produtos para adicionar ao estoque: ");
+		quantity = sc.nextInt();
+		System.out.println("");
+		produto.addProducts(quantity);
+
 		System.out.println("Dados atualizados Produto: " + produto.toString());
 		System.out.println("");
-		
+
 		System.out.print("Digite o número de produtos para remover do estoque: ");
 		quantity = sc.nextInt();
 		produto.removeProducts(quantity);
 		System.out.println("");
-		
+
 		System.out.printf("Dados atualizados Produto: " + produto.toString());
 
 		sc.close();
