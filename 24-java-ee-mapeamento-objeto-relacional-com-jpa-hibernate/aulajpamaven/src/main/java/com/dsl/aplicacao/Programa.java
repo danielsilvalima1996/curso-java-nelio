ad0cf8b0c52545837b16bf6f1e489a678947f4c9
danielsilvalima1996 <dsl15021996@gmail.com>
2020-12-15 17:27:29 -0300

    24:Java EE - Mapeamento objeto-relacional com

---
+++ 24-java-ee-mapeamento-objeto-relacional-com-jpa-hibernate/aulajpamaven/src/main/java/com/dsl/aplicacao/Programa.java
@@ -0,0 +1,27 @@
+package com.dsl.aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.dsl.dominio.Pessoa;
+
+public class Programa {
+    
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        Pessoa p = em.find(Pessoa.class, 2);
+
+        em.remove(p);
+        em.getTransaction().commit();
+
+        System.out.println("Pronto!");
+        em.close();
+        emf.close();
+        
+    }
+
+}
